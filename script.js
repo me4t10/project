1622a28fe22200ce147fd53cc6a0285cd92d7479
Maksim <me4t10@gmail.com>
2020-07-21 18:43:36 +0700

    Beatify

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 
-let money = +prompt ("Ваш бюджет на месяц?", ""),
-    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
-    
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
 let appData = {
     budget: money,
     timeData: time,
@@ -15,79 +15,34 @@ let appData = {
 
 
 for (let i = 0; i < 2; i++) {
-    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt ("Во сколько обойдется?", "");
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
 
-    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
 
-        console.log ("done");
+        console.log("done");
 
         appData.expenses[a] = b;
-    } else {                            
-        console.log ("bad result");
+    } else {
+        console.log("bad result");
         i--;
     }
 
 }
 
 
-
-// Используем цикл WHILE
-
-// let i = 0;
-// while (i < 2) {
-//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt ("Во сколько обойдется?", "");
-
-//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-
-//         console.log ("done");
-
-//         appData.expenses[a] = b;
-//     } else {
-//          console.log ("bad result");
-//          i--;
-//     }
-
-//     i++;
-// }
-
-
-
-// Используем цикл DO...WHILE
-
-// let i = 0;
-// do {
-//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt ("Во сколько обойдется?", "");
-
-//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-
-//         console.log ("done");
-
-//         appData.expenses[a] = b;
-//     } else {
-//          console.log ("bad result");
-//          i--;
-//     }
-
-//     i++;
-// }
-// while(i < 2);
-
-
 appData.moneyPerDay = appData.budget / 30;
 
 
-alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
 
 
 if (appData.moneyPerDay < 100) {
-    console.log ("Это минимальный уровень достатка!");
+    console.log("Это минимальный уровень достатка!");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log ("Это средний уровень достатка!");
+    console.log("Это средний уровень достатка!");
 } else if (appData.moneyPerDay > 2000) {
-    console.log ("Это высокий уровень достатка!");
+    console.log("Это высокий уровень достатка!");
 } else {
-    console.log ("Произошла ошибка");
+    console.log("Произошла ошибка");
 }
